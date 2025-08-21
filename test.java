4371228de92d0c8915c24179a50c26321ed651c2
sripadkaparthi777 <kishoreazmeera2004@gmail.com>
2025-08-21 12:30:17 +0530

    java program

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
 import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = scanner.nextInt();
+
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + num + " is " + factorial);
+    }
+}
+76
